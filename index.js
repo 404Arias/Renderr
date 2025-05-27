cac6facf3c7f396a2f85057395316feaf3787ee2
404arias <207381286+404Arias@users.noreply.github.com>
2025-05-27 14:43:07 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,121 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const cors = require("cors");
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+app.use(cors());
+
+app.use(express.json());
+// Token personalizado para mostrar el body en POST
+morgan.token("body", (req) => {
+  return req.method === "POST" ? JSON.stringify(req.body) : "";
+});
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
+// Función para generar un id aleatorio
+function generateRandomId() {
+  return Math.floor(Math.random() * 1000000);
+}
+
+// Nuevo endpoint POST para agregar personas
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  // Si falta el nombre o el número
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "name or number missing",
+    });
+  }
+
+  // Si el nombre ya existe
+  if (persons.find((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: "name must be unique",
+    });
+  }
+
+  const newPerson = {
+    id: generateRandomId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(newPerson);
+
+  response.json(newPerson);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+app.get("/info", (request, response) => {
+  const data = info();
+  response.send(
+    `<p>Phonebook has info for ${data.total} people</p><p>${data.date}</p>`
+  );
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id); // corregido
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id); // corregido
+  response.status(204).end();
+});
+
+function info() {
+  const now = new Date();
+  return {
+    total: persons.length,
+    date: now.toLocaleString("es-ES", { timeZoneName: "short" }),
+  };
+}
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
